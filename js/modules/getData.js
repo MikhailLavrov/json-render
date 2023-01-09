041508a63dc79a53ccfba29d0980292a2b532949
Mikhail Lavrov <lavrov.fd@gmail.com>
2023-01-09 20:47:21 +0400

    newset

--- js/modules/getData.js
+++ js/modules/getData.js
@@ -1,8 +1,10 @@
+const DATA_URL = '../data.json';
+
 const getData = (onSuccess) => {
-  fetch('../../data.json')
+  fetch(DATA_URL)
     .then(response => response.ok ? response.json() : console.log(`${response.status} — ${response.statusText}`))
     .then(items => onSuccess(items))
-    .catch(() => alert('Damn! Not today, bro :('))
+    .catch(() => alert('Damn! ʕ•́ᴥ•̀ʔ'))
 };
 
 export {getData};
